f34a18930e4b39520c39ba4f690fbe18dfb3dbb9
Zidan Niyas <muhdzidan007@gmail.com>
2024-07-18 09:54:03 +0530

    added the functionality to change the number of grids based on input

--- scripts.js
+++ scripts.js
@@ -1,27 +1,38 @@
 // grids = number of rows and columns we need
+// gridContainer = the container where the rows need to be added
 // gridSize = the grid size everytime the input field is changed
 
 const gridContainer = document.querySelector("#grid-container");
-
+const gridSize = document.querySelector("#gridSize");
 let grids = 1;
 console.log(grids);
-const gridSize = document.querySelector("#gridSize");
-gridSize.addEventListener('input', (event) => {
-    grids = event.target.value;
-    console.log(grids);
-});
 
 const addRows = (grids) => {
     for(let i=1; i<=grids; i++) {
-        addCols(grids);
+        const row = document.createElement("div");
+        row.classList.add("row");
+        console.log(`row ${i} added`);
+        for(let j=1; j<=grids; j++) {
+            const col = document.createElement("div");
+            col.classList.add("col");
+            row.appendChild(col);
+            console.log(`col ${j} of row ${i} added`);
+        }
+        gridContainer.appendChild(row);
     }
 }
 
-const addCols = (grids) => {
-    for(let i=1; i<=grids; i++) {
-        const col = document.createElement("div");
-        col.classList.add("col");
+
+gridSize.addEventListener('input', (event) => {
+    grids = event.target.value;
+    console.log(grids);
+    while(gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
     }
-}
+    addRows(grids);
+});
+
+
+
 
 
