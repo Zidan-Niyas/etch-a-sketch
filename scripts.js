685fc295b41ea2dc4fd97912f0de4446626ec262
Zidan Niyas <muhdzidan007@gmail.com>
2024-07-19 01:22:19 +0530

    added normal and rainbow mode

--- scripts.js
+++ scripts.js
@@ -4,23 +4,48 @@
 
 const gridContainer = document.querySelector("#grid-container");
 const gridSize = document.querySelector("#gridSize");
-
+const rainbow = document.querySelector("#rainbow");
+const normal = document.querySelector("#normal");
+const reset = document.querySelector("#reset");
 const color = document.querySelector("#chooseColor");
+
 console.log(color);
 
 let grids = 1;
-console.log(grids);
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
 const addRows = (grids) => {
     for(let i=1; i<=grids; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
-        console.log(`row ${i} added`);
         for(let j=1; j<=grids; j++) {
             const col = document.createElement("div");
             col.classList.add("col");
             row.appendChild(col);
-            console.log(`col ${j} of row ${i} added`);
+            col.addEventListener('mouseover', (e) => {
+                e.target.style.backgroundColor = color.value;
+            });
+            // Normal Mode
+            normal.addEventListener("click", () => {
+                col.addEventListener('mouseover', (e) => {
+                    e.target.style.backgroundColor = color.value;
+                });
+            });
+            // Rainbow Mode
+            rainbow.addEventListener("click", () => {
+                col.addEventListener('mouseover', (e) => {
+                    const randomColor = getRandomColor();
+                    e.target.style.backgroundColor = randomColor;
+                })
+            });
         }
         gridContainer.appendChild(row);
     }
@@ -38,11 +63,9 @@ gridSize.addEventListener('input', (event) => {
 
 color.addEventListener('input', (event) => {
     color = event.target.value;
-})
+});
 
-col.addEventListener("mouseover", () => {
-    col.style.backgroundColor = color;
-})
+
 
 
 
